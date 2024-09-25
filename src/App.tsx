03bd60e0e4d696f78f378e5da40fbe507db3a164
yuukur <yuukuurii@gmail.com>
2024-09-26 00:28:37 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="w-40 text-blue-300 bg-white-600">こんにちは</h1>
+    </div>
+  );
+}
+
+export default App;

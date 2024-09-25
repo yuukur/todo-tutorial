c2207d2d222a4bbb2e94d46cafcaf55ec43390c0
yuukur <yuukuurii@gmail.com>
2024-09-26 01:42:35 +0900

    add:新しいTODOを作成する関数

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./App.css";
 import "./index.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //新しいTODOを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Todoリスト with Typescript</h2>
-        <form onSubmit={() => {}}>
-          <input type="text" onChange={() => {}} className="" />
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className="" />
           <input type="submit" value="作成" />
         </form>
       </div>
